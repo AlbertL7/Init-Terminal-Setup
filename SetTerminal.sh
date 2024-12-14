9cf7a57ff2c76d9c2ec12f6e413f01cdf2488e5b
BlueSynAck <71300144+AlbertL7@users.noreply.github.com>
2024-12-14 01:57:59 -0800

    Update SetTerminal.sh

--- SetTerminal.sh
+++ SetTerminal.sh
@@ -2,7 +2,7 @@
 
 # Ensure the script is run as root
 if [ "$(id -u)" -ne 0 ]; then
-  echo "This script must be run as root. Use sudo or log in as root."
+  echo "This script must be run as root. Use sudo or log in as root. You should install 'zsh' before running"
   exit 1
 fi
 
